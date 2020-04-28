a1fc9a205a594798b540b298583ce5b68500c8cf
Yasuaki Uechi <y@uechi.io>
2020-04-28 16:04:34 +0900

    cosmetic changes

--- src/main.rs
+++ src/main.rs
@@ -15,56 +15,49 @@ fn main() {
     let mut bytes = content.bytes();
     while let Some(b) = bytes.next() {
         if b & 0x80 == 0 {
-            // ASCII
+            // if MSB is 0, is is ASCII.
             println!(
-                "{} {} {}",
+                "{} {} {} (U+{:04X})",
                 "├".bright_black(),
                 format!("{:08b}", b).cyan(),
                 std::str::from_utf8(&[b])
                     .unwrap()
                     .replace("\x0a", "↵")
-                    .replace("\x20", "<space>")
+                    .replace("\x20", "<space>"),
+                b
             );
         } else {
             let nb = (!b).leading_zeros();
             let bin_str = format!("{:08b}", b);
             let (first_bin, second_bin) = bin_str.split_at(nb as usize + 1);
-            println!(
-                "{} {}{}",
-                "├┬".bright_black(),
-                first_bin.magenta(),
-                second_bin.green()
-            );
+            print!("{} ", "├".bright_black());
+            print!("{}{}", first_bin.magenta(), second_bin.green(),);
             let mut v: Vec<u8> = Vec::new();
             let mut f: u32 = 0;
             let base = 6 * (nb - 1);
+
             v.push(b);
+
             f |= (b as u32 & (u32::pow(2, 7 - nb) - 1)) << base;
             for i in 0..(nb - 1) {
                 let next_byte = bytes.next().unwrap();
+                // if first 2 MSB are not '10', it's illegal sequence.
                 if next_byte & 0xC0 != 0x80 {
                     println!("Illegal byte");
                     process::exit(1);
                 }
                 let bin_str = format!("{:08b}", next_byte);
                 let (first_bin, second_bin) = bin_str.split_at(2);
-                println!(
-                    "{} {}{}",
-                    "│├".bright_black(),
-                    first_bin,
-                    second_bin.green()
-                );
+                print!("{}{}", first_bin.bright_black(), second_bin.green());
                 f |= (next_byte as u32 & 0x3f) << base - (i + 1) * 6;
                 v.push(next_byte);
             }
             println!(
-                "{}   {} {} (U+{:x} - {} bytes) {}",
-                "│└".bright_black(),
+                " ({} bytes) = {} {} (U+{:04X})",
+                nb,
                 format!("{:0b}", f).cyan(),
                 std::str::from_utf8(&v[..]).unwrap().bold(),
-                f,
-                nb,
-                format!("https://www.compart.com/en/unicode/U+{:x}", f).bright_black()
+                f
             );
         }
     }
